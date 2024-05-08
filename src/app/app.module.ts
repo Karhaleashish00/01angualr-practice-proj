d39f414cc7f3ea96b30f977782eaae0d00c34f22
Ashish <karhaleashish007@gmail.com>
2024-05-08 15:29:06 +0530

    bootstrap changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CustomerRegisterComponent } from './components/register/customer-register/customer-register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component:  LandingPageComponent  },
+  { path: 'register', component:  RegisterComponent  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    RegisterComponent,
+    CustomerRegisterComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)],
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
